d5f1ef60a13f4d194e43ae7c40d00e63129fcb3f
Heshan Asmadala <Heshanasmadala@gmail.com>
2025-11-03 17:19:44 +0530

    Database connected

---
+++ server/controllers/userController.js
@@ -0,0 +1,32 @@
+import  jwt  from "jsonwebtoken";
+import User from "../models/user.js";
+
+// Generate JWT
+const generateToken = (id)=>{
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn:'30d'
+    })
+}
+
+// API to register user
+export const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const userExists = await User.findOne({email})
+
+        if(userExists){
+            return res.json({success: false, message: "User ALready exists"})
+        }
+
+        const user = await User.create({name, email, password })
+
+        const token = generateToken(user._id)
+        res.json({success:true, token})
+    } catch (error) {
+        return res.json({success:false, message:error.message})
+    }
+}
+
+
+// API to user login
